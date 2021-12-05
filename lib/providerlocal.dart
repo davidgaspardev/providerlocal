480d916104fefd33936417afda23fdc2af03bf9e
David Gaspar <davidgaspar.dev@gmail.com>
2021-12-05 00:56:06 -0300

    Exporting plugin

--- lib/providerlocal.dart
+++ lib/providerlocal.dart
@@ -1,17 +1,2 @@
-// You have generated a new plugin project without
-// specifying the `--platforms` flag. A plugin project supports no platforms is generated.
-// To add platforms, run `flutter create -t plugin --platforms <platforms> .` under the same
-// directory. You can also find a detailed instruction on how to add platforms in the `pubspec.yaml` at https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
-
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class Providerlocal {
-  static const MethodChannel _channel = MethodChannel('providerlocal');
-
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
+export 'package:providerlocal/src/controller.dart';
+export 'package:providerlocal/src/providerlocal.dart';
